37e5764c78c788f689952d3fc30e521ddd5b79c4
Luis Gustavo Bier Schüler <luis.schuler@gmail.com>
2016-06-19 15:45:47 -0300

    Criação das entidades

---
+++ SisMed.Domain/Entities/TempoConsulta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisMed.Domain.Entities
+{
+    class TempoConsulta
+    {
+        public int Id { get; set; }
+        public int IdMedico { get; set; }
+        public virtual Medico Medico { get; set; }
+        public int IdTipoConsulta { get; set; }
+        public virtual TipoConsulta TipoConsulta { get; set; }
+        public double TempoMedio { get; set; }
+    }
+}
