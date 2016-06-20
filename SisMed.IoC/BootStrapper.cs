6b28b368b91ddbb637e908965c8e73d07aff224f
Luis Gustavo Bier Schüler <luis.schuler@gmail.com>
2016-06-19 23:18:56 -0300

    Interfaces for everyone

--- SisMed.IoC/BootStrapper.cs
+++ SisMed.IoC/BootStrapper.cs
@@ -29,6 +29,26 @@ namespace SisMed.IoC
             container.Register<ICidadeAppService, CidadeAppService>(Lifestyle.Scoped);
             container.Register<ICidadeService, CidadeService>(Lifestyle.Scoped);
             container.Register<ICidadeRepository, CidadeRepository>(Lifestyle.Scoped);
+
+            container.Register<IConsultaAppService, ConsultaAppService>(Lifestyle.Scoped);
+            container.Register<IConsultaService, ConsultaService>(Lifestyle.Scoped);
+            container.Register<IConsultaRepository, ConsultaRepository>(Lifestyle.Scoped);
+
+            container.Register<IEspecialidadeAppService, EspecialidadeAppService>(Lifestyle.Scoped);
+            container.Register<IEspecialidadeService, EspecialidadeService>(Lifestyle.Scoped);
+            container.Register<IEspecialidadeRepository, EspecialidadeRepository>(Lifestyle.Scoped);
+
+            container.Register<IMedicoAppService, MedicoAppService>(Lifestyle.Scoped);
+            container.Register<IMedicoService, MedicoService>(Lifestyle.Scoped);
+            container.Register<IMedicoRepository, MedicoRepository>(Lifestyle.Scoped);
+
+            container.Register<ITempoConsultaAppService, TempoConsultaAppService>(Lifestyle.Scoped);
+            container.Register<ITempoConsultaService, TempoConsultaService>(Lifestyle.Scoped);
+            container.Register<ITempoConsultaRepository, TempoConsultaRepository>(Lifestyle.Scoped);
+
+            container.Register<ITipoConsultaAppService, TipoConsultaAppService>(Lifestyle.Scoped);
+            container.Register<ITipoConsultaService, TipoConsultaService>(Lifestyle.Scoped);
+            container.Register<ITipoConsultaRepository, TipoConsultaRepository>(Lifestyle.Scoped);
         }
     }
 }
