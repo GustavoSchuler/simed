cbef91577e3cdffea26119aa38af30e8fd199898
Luis Gustavo Bier Schüler <luis.schuler@gmail.com>
2016-06-26 11:29:19 -0300

    TempoConsulta - arquivos

---
+++ SisMed.WebUI/Controllers/TempoConsultaController.cs
@@ -0,0 +1,103 @@
+﻿using AutoMapper;
+using SisMed.Application.Interface;
+using SisMed.Domain.Entities;
+using SisMed.WebUI.Security;
+using SisMed.WebUI.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SisMed.WebUI.Controllers
+{
+    [BasicAuth(Roles = "ADMIN")]
+    public class TempoConsultaController : Controller
+    {
+        private readonly ITempoConsultaAppService mTempoConsultaApp;
+
+        public TempoConsultaController(ITempoConsultaAppService tempoConsultaApp)
+        {
+            mTempoConsultaApp = tempoConsultaApp;
+        }
+
+        // GET: TempoConsulta
+        public ActionResult Index()
+        {
+            var clienteViewModel = Mapper.Map<IEnumerable<TempoConsulta>, IEnumerable<TempoConsultaViewModel>>(mTempoConsultaApp.GetAll());
+            return View(clienteViewModel);
+        }
+
+        // GET: TempoConsulta/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TempoConsulta/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TempoConsulta/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TempoConsultaViewModel tempoConsulta)
+        {
+            if (ModelState.IsValid)
+            {
+                var clienteDomain = Mapper.Map<TempoConsultaViewModel, TempoConsulta>(tempoConsulta);
+                mTempoConsultaApp.Add(clienteDomain);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(tempoConsulta);
+        }
+
+        // GET: TempoConsulta/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var tempoConsulta = mTempoConsultaApp.GetById(id);
+            var TempoConsultaViewModel = Mapper.Map<TempoConsulta, TempoConsultaViewModel>(tempoConsulta);
+
+            return View(TempoConsultaViewModel);
+        }
+
+        // POST: TempoConsulta/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(TempoConsultaViewModel tempoConsulta)
+        {
+            if (ModelState.IsValid)
+            {
+                var tempoConsultaDomain = Mapper.Map<TempoConsultaViewModel, TempoConsulta>(tempoConsulta);
+                mTempoConsultaApp.Update(tempoConsultaDomain);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: TempoConsulta/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var tempoConsulta = mTempoConsultaApp.GetById(id);
+            var TempoConsultaViewModel = Mapper.Map<TempoConsulta, TempoConsultaViewModel>(tempoConsulta);
+
+            return View(TempoConsultaViewModel);
+        }
+
+        // POST: TempoConsulta/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmado(int id)
+        {
+            var tempoConsulta = mTempoConsultaApp.GetById(id);
+            var TempoConsultaViewModel = Mapper.Map<TempoConsulta, TempoConsultaViewModel>(tempoConsulta);
+
+            mTempoConsultaApp.Remove(tempoConsulta);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SisMed.WebUI/ViewModel/TempoConsultaViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SisMed.WebUI.ViewModel
+{
+    public class TempoConsultaViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int IdMedico { get; set; }
+
+        public int IdTipoConsulta { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo tempo médio")]
+        public double TempoMedio { get; set; }
+    }
+}
